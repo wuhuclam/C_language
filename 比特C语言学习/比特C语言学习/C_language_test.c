bde97a3821c00eb81922aac93ae5fa6142212cc0
Small black human <2575381447@qq.com>
2022-10-29 14:06:37 +0800

    2022.10.29

--- 比特C语言学习/比特C语言学习/C_language_test.c
+++ 比特C语言学习/比特C语言学习/C_language_test.c
@@ -6115,3 +6115,109 @@
 //    printf("%d %d", i_max_x + 1, i_max_y + 1);
 //    return  0;
 //}
+
+//判断两个二维数组是否相等
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//    //获取变长二维数组的大小
+//    int i_size_x = 0;
+//    int i_size_y = 0;
+//    scanf("%d %d", &i_size_x, &i_size_y);
+//
+//    //声明两个变长数组,只有支持c99的编译器才支持变长数组，变长数组不可以初始化
+//    int i_number_one[i_size_x][i_size_y];
+//    int i_number_two[i_size_x][i_size_y];
+//
+//    //获取数组中的值
+//    int i_i = 0;
+//    int i_j = 0;
+//    for (i_i = 0; i_i < i_size_x; i_i++)
+//    {
+//        for (i_j = 0; i_j < i_size_y; i_j++)
+//        {
+//            scanf("%d", &i_number_one[i_i][i_j]);
+//        }
+//    }
+//    for (i_i = 0; i_i < i_size_x; i_i++)
+//    {
+//        for (i_j = 0; i_j < i_size_y; i_j++)
+//        {
+//            scanf("%d", &i_number_two[i_i][i_j]);
+//        }
+//    }
+//
+//    //开始比较
+//    int i_flag = 1;
+//    for (i_i = 0; i_i < i_size_x; i_i++)
+//    {
+//        for (i_j = 0; i_j < i_size_y; i_j++)
+//        {
+//            if ((i_number_one[i_i][i_j] != i_number_two[i_i][i_j]))
+//            {
+//                i_flag = 0;
+//                break;
+//            }
+//        }
+//    }
+//
+//    //输出结果
+//    if (i_flag)
+//    {
+//        printf("Yes\n");
+//    }
+//    else
+//    {
+//        printf("No\n");
+//    }
+//    return  0;
+//}
+
+//矩阵换行和列
+//#include<stdio.h>
+//int main(void) {
+//    int m = 0, n = 0;
+//    scanf("%d %d", &m, &n);
+//    int s[m][n];
+//    //int ss[n][m];//一开始想用两个矩阵，发现后面如果多组输入，更改的一直是初始矩阵，且代码繁杂,采用一个矩阵的方式
+//    for (int i = 0; i < m; i++) {//初始化
+//        for (int j = 0; j < n; j++) {
+//            scanf("%d ", &s[i][j]);
+//        }
+//    }
+//    int t = 0;//变换次数
+//    char flag = 'r';//判断行还是列变
+//    int c1 = 0, c2 = 0;//变换的行（列）号
+//    int temp = 0;
+//    scanf("%d ", &t);
+//    while (t) {
+//        scanf(" %c %d %d", &flag, &c1, &c2);
+//        c1--; c2--;//给的是行号，--之后才是数组下标值
+//        if (flag == 'r') {
+//            for (int j = 0; j < n; j++) {
+//                temp = s[c2][j];//交换c1和c2行
+//                s[c2][j] = s[c1][j];
+//                s[c1][j] = temp;
+//            }
+//        }
+//
+//        if (flag == 'c') {
+//            for (int i = 0; i < m; i++) {//交换C1和C2列
+//                temp = s[i][c2];
+//                s[i][c2] = s[i][c1];
+//                s[i][c1] = temp;
+//            }
+//        }
+//        t--;
+//    }
+//    //打印最终数组
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++)
+//            printf("%d ", s[i][j]);
+//        printf("\n");
+//    }
+//
+//    return 0;
+//}
+
