7113406002079e538399f285bacb07d63ad6e997
Small black human <2575381447@qq.com>
2022-12-02 22:07:19 +0800

    2022.12.2

---
+++ 比特C语言学习/比特C语言学习/test.c
@@ -0,0 +1,13 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("Holle word!");
+//	return  0;
+//}if((c_string[i_i] > 65 && c_string[i_i] < 90 || c_string[]))
+//if (c_flag == c_string[i_i] || c_flag == c_string[i_i] + 32 || c_flag == c_string[i_i] - 32)
+//{
+//    i_count++;
+//}
