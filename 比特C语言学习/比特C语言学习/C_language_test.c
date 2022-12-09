98c2809a80d8a9f2c1fcea4f3a790805e3d1f916
Small black human <2575381447@qq.com>
2022-12-09 13:29:13 +0800

    2022.12.9

--- 比特C语言学习/比特C语言学习/C_language_test.c
+++ 比特C语言学习/比特C语言学习/C_language_test.c
@@ -8270,17 +8270,30 @@
 //}
 
 //打印c字形
-#include <stdio.h>
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("    ****\n"
+//		   "************\n"
+//	       "***\n"
+//	       "*\n"
+//	       "***\n"
+//		   "************\n"
+//		   "    ****\n");
+//
+//	return  0;
+//}
 
-int main(void)
+//打印特殊图形
+#include<stdio.h>
+int main()
 {
-	printf("    ****\n"
-		   "************\n"
-	       "***\n"
-	       "*\n"
-	       "***\n"
-		   "************\n"
-		   "    ****\n");
-
-	return  0;
+    char a = 176, b = 219;
+    printf("%c%c%c%c%c\n", b, a, a, a, b);
+    printf("%c%c%c%c%c\n", a, b, a, b, a);
+    printf("%c%c%c%c%c\n", a, a, b, a, a);
+    printf("%c%c%c%c%c\n", a, b, a, b, a);
+    printf("%c%c%c%c%c\n", b, a, a, a, b);
+    return 0;
 }
